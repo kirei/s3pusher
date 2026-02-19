7ec26f8a7b1f53395d8fccd8e2d39a5691d18ee7
Jakob Schlyter <jakob@kirei.se>
2026-02-19 21:09:14 +0100

    Use structlog

--- s3pusher.py
+++ s3pusher.py
@@ -6,52 +6,55 @@ from datetime import UTC, datetime
 from pathlib import Path
 
 import boto3
+import structlog
 from watchdog.events import FileModifiedEvent, FileSystemEvent, FileSystemEventHandler
 from watchdog.observers import Observer
 
 STABLE_FILE_DELAY_SECONDS = 1
 EXCEPTION_DELAY_SECONDS = 60
 
+logger = structlog.get_logger()
 
-class FileEventHandler(FileSystemEventHandler):
+
+class ThePusher(FileSystemEventHandler):
     def __init__(self, bucket: str | None, hostname: str | None) -> None:
         self.bucket = bucket
         self.hostname = hostname
+        self.logger = structlog.get_logger()
 
     def on_any_event(self, event: FileSystemEvent) -> None:
-        logging.debug("Event detected: %s", event)
+        self.logger.debug("Event %s detected", event)
 
         if isinstance(event, FileModifiedEvent):
             modified_file = Path(event.src_path)
 
-            if not modified_file.is_file():
-                logging.debug("%s does not exist, skipped", modified_file)
-                return
+            with structlog.contextvars.bound_contextvars(filename=str(modified_file)):
+                if not modified_file.is_file():
+                    return
 
-            logging.info("File modified: %s", modified_file)
+                self.logger.info("File modified")
 
-            if self.wait_for_stable_file(modified_file):
-                logging.debug("File is stable: %s", modified_file)
-
-                try:
-                    s3_object_key = self.get_s3_object_key(hostname=self.hostname, filename=modified_file)
-                    if self.bucket:
-                        logging.info(
-                            "Uploading %s as %s/%s",
-                            modified_file,
-                            self.bucket,
-                            s3_object_key,
-                        )
-                        s3_client = boto3.client("s3")
-                        with open(modified_file, "rb") as fp:
-                            s3_client.upload_fileobj(fp, self.bucket, s3_object_key)
-                        modified_file.unlink()
-                        logging.info("File uploaded and deleted: %s", modified_file)
-                    else:
-                        logging.warning("File upload skipped: %s", modified_file)
-                except Exception as exc:
-                    logging.error("Failed to upload %s: %s", modified_file, exc)
-                    time.sleep(EXCEPTION_DELAY_SECONDS)
+                if self.wait_for_stable_file(modified_file):
+                    try:
+                        s3_object_key = self.get_s3_object_key(hostname=self.hostname, filename=modified_file)
+                        if self.bucket:
+                            with structlog.contextvars.bound_contextvars(
+                                s3_bucket=self.bucket, s3_object_key=s3_object_key
+                            ):
+                                self.logger.debug("Uploading file")
+                                s3_client = boto3.client("s3")
+                                t1 = time.time()
+                                with open(modified_file, "rb") as fp:
+                                    s3_client.upload_fileobj(fp, self.bucket, s3_object_key)
+                                t2 = time.time()
+                                self.logger.info("File uploaded", s3_upload_seconds=round(t2 - t1, 3))
+                            modified_file.unlink()
+                            self.logger.info("File deleted")
+                        else:
+                            self.logger.warning("File upload skipped")
+                    except Exception as exc:
+                        self.logger.error("Failed to upload", exception=str(exc))
+                        time.sleep(EXCEPTION_DELAY_SECONDS)
 
     @staticmethod
     def get_s3_object_key(filename: Path | None = None, hostname: str | None = None) -> str:
@@ -72,8 +75,7 @@ class FileEventHandler(FileSystemEventHandler):
             fields_list.append(filename.name)
         return "/".join(fields_list)
 
-    @staticmethod
-    def wait_for_stable_file(filename: Path, timeout: int = 60) -> bool:
+    def wait_for_stable_file(self, filename: Path, timeout: int = 60) -> bool:
         """Wait for a file to become stable (not modified for a certain period)"""
 
         start_time = time.time()
@@ -83,11 +85,15 @@ class FileEventHandler(FileSystemEventHandler):
             try:
                 current_size = filename.stat().st_size
                 if current_size == last_size:
+                    self.logger.debug("File is stable")
                     return True
                 last_size = current_size
             except FileNotFoundError:
                 return False
+            self.logger.debug("Waiting for file to become stable", filename=str(filename))
             time.sleep(STABLE_FILE_DELAY_SECONDS)
+
+        self.logger.warning("File not stable, timeout reached")
 
         return False
 
@@ -104,23 +110,34 @@ def main():
         default=None,
         help="Hostname to include in S3 object key",
     )
+    parser.add_argument("--log-json", action="store_true", help="Log in JSON format")
     parser.add_argument("--debug", action="store_true", help="Enable debugging")
 
     args = parser.parse_args()
 
-    if args.debug:
-        logging.basicConfig(level=logging.DEBUG)
-    else:
-        logging.basicConfig(level=logging.INFO)
+    structlog.configure(
+        processors=[
+            structlog.contextvars.merge_contextvars,
+            structlog.processors.add_log_level,
+            structlog.processors.StackInfoRenderer(),
+            structlog.dev.set_exc_info,
+            structlog.processors.TimeStamper(fmt="%Y-%m-%dT%H:%M:%SZ", utc=True),
+            structlog.processors.JSONRenderer() if args.log_json else structlog.dev.ConsoleRenderer(),
+        ],
+        wrapper_class=structlog.make_filtering_bound_logger(logging.DEBUG if args.debug else logging.INFO),
+        context_class=dict,
+        logger_factory=structlog.PrintLoggerFactory(),
+        cache_logger_on_first_use=False,
+    )
 
     if not args.bucket:
-        logging.warning("No S3 bucket specified, file upload will be skipped")
+        logger.warning("No S3 bucket specified, file upload will be skipped")
 
-    logging.info("Watching directory: %s", args.directory)
+    logger.info("Watching directories for changes", directories=args.directory)
 
-    event_handler = FileEventHandler(bucket=args.bucket, hostname=args.hostname)
-
+    event_handler = ThePusher(bucket=args.bucket, hostname=args.hostname)
     observer = Observer()
+
     for directory in args.directory:
         observer.schedule(event_handler, directory)
     observer.start()
